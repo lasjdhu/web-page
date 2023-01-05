2763de561e834088241727ec3bce95e61201408c
Dmitrii Ivanushkin <dmitrii.unwritten183@passmail.net>
2023-01-05 14:53:21 +0100

    build(navbar): header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,57 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { FiMenu } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
 
 function Navbar() {
-    return (   
-    <nav class="p-3">
-      <div class="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="#"><span class="self-center text-xl font-semibold whitespace-nowrap">Flowbite</span></a>
-        <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700" id="language-dropdown-menu">
-          <ul class="py-1" role="none">
-            <li>
-              <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">
-                <div class="inline-flex items-center">
-                    English (US)
+    const [toggle, setToggle] = useState(false);
+    const [smallScreen, setSmallScreen] = useState(false);
+
+    const handleClick = () => {
+        setToggle(!toggle);
+    };
+
+    useEffect(() => {
+        const media = window.matchMedia("(max-width: 1023px)");
+        media.addListener(handleMediaChange);
+        handleMediaChange(media);
+
+        return () => {
+            media.removeListener(handleMediaChange);
+        };
+    }, []);
+
+    const handleMediaChange = media => {
+        if (media.matches){
+            setSmallScreen(true);
+        }
+        else{
+            setSmallScreen(false);
+        }
+    };
+
+    return (
+        <nav class="flex items-center justify-between flex-wrap p-10">
+            <div class="flex items-center flex-shrink-0 mr-16">
+                <span class="font-semibold text-4xl tracking-tight">DI</span>
+            </div>
+            <div class="block lg:hidden">
+                <button 
+                class="flex items-center px-3 py-2 border hover:bg-stone-100 hover:border-stone-100 hover:text-stone-900"
+                onClick={handleClick}>
+                    {toggle ?
+                    (<MdClose className=""/>) :
+                    (<FiMenu className=""/>)}
+                </button>
+             </div>
+            {(!smallScreen || toggle) &&
+            <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                <div class="lg:flex-grow">
+                    <a href="#" class="block mt-4 lg:inline-block lg:mt-0 mr-4">
+                        Home
+                    </a>
+                    <a href="#about" class="block mt-4 lg:inline-block lg:mt-0 mr-4">
+                        About
+                    </a>
+                    <a href="#projects" class="block mt-4 lg:inline-block lg:mt-0">
+                        Projects
+                    </a>
                 </div>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">
-                <div class="inline-flex items-center">
-                  <svg class="h-3.5 w-3.5 rounded-full mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" id="flag-icon-css-de" viewBox="0 0 512 512"><path fill="#ffce00" d="M0 341.3h512V512H0z"/><path d="M0 0h512v170.7H0z"/><path fill="#d00" d="M0 170.7h512v170.6H0z"/></svg>
-                  Deutsch
-                </div>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">M</button>
-        <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-          <ul class="flex flex-col mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-          <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 
-                md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 
-                md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 
-                md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 
-                md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 
-                md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 
-                md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                Projects
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+                <a 
+                href="#contact" 
+                class="inline-block px-4 py-2 leading-none border text-white border-stone-100 hover:border-transparent hover:bg-stone-100 hover:text-stone-900 mt-4 lg:mt-0">
+                    Contact
+                </a>
+            </div>
+            }
+        </nav>
     );
 }
 
