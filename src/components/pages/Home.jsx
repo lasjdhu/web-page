9085efc731c94788e326fe55535981d77c686062
Dmitrii Ivanushkin <dmitrii.ivanushkin1@gmail.com>
2023-01-29 18:59:38 +0100

    fix(Home): hr

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Home() {
     return (
-        <div className="m-auto">
-            <h1 className="text-center lg:text-8xl text-6xl pb-5">Welcome.</h1>
-            <hr className="lg:m-auto my-5" />
-            <h1 className="text-center lg:text-5xl text-4xl pt-5">My name is Dmitrii Ivanushkin</h1>
-            <h1 className="text-center lg:text-5xl text-4xl pt-5">I am a software developer</h1>
+        <div className="m-auto text-center">
+            <h1 className="lg:text-8xl text-6xl pb-5">Welcome.</h1>
+            <hr className="lg:m-auto mx-10"/>
+            <h1 className="lg:text-5xl text-4xl pt-5">My name is Dmitrii Ivanushkin</h1>
+            <h1 className="lg:text-5xl text-4xl pt-5">I am a software developer</h1>
         </div>
     );
 }
