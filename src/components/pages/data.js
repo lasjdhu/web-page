9b19ef608c6d58f2e91494d24fa3aba24b6a4994
Dmitrii Ivanushkin <dmitrii.ivanushkin1@gmail.com>
2023-02-04 12:50:42 +0100

    test(Loading, Transition)

---
+++ src/components/pages/data.js
@@ -0,0 +1,46 @@
+export const langs = [{
+    id: 0,
+    name: 'C/C++'
+  }, {
+    id: 1,
+    name: 'HTML, CSS'
+  }, {
+    id: 2,
+    name: 'JavaScript'
+  }, {
+    id: 3,
+    name: 'Python'
+  }, {
+    id: 4,
+    name: 'PostgreSQL'
+}];
+
+export const frameworks = [{
+    id: 0,
+    name: 'React'
+  }, {
+    id: 1,
+    name: 'TailwindCSS'
+  }, {
+    id: 2,
+    name: 'Django'
+}];
+
+export const others = [{
+    id: 0,
+    name: 'Git'
+  }, {
+    id: 1,
+    name: 'Linux administrating'
+  }, {
+    id: 2,
+    name: 'UML'
+}];
+
+export const edu = [{
+    id: 0,
+    name: 'Business High School'
+  }, {
+    id: 1,
+    name: 'Brno University of Technology'
+}];
---
+++ src/components/reusable/Badge.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaHammer } from "react-icons/fa";
+import { useTranslation } from 'react-i18next';
+
+function Badge() {
+    const { t } = useTranslation();
+    
+	return (
+		<div className="flex p-3 animate-pulse justify-center">
+			<FaHammer size="1.5em"/>
+			<p className="text-xl pl-5">{t('Coming soon')} . . .</p>
+		</div>
+	);
+}
+
+export default Badge;
