e5dfd3826527cf972c6b8c29b615ec11fc56b7e0
Dmitrii Ivanushkin <dmitrii.ivanushkin1@gmail.com>
2023-02-05 21:44:02 +0100

    fix: libraries :)

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -10,21 +10,22 @@ function About() {
         <main className="m-auto">
             <section className="lg:flex justify-center">
                 <div className="mt-10">
-                    <p className="text-xl mb-3 font-bold tracking-widest">{t('EDUCATION')}</p>
+                    <h2 className="text-xl mb-3 font-bold tracking-widest">{t('EDUCATION')}</h2>
                     <List props={edu} />
                 </div>
             </section>
             <section className="lg:flex justify-center">
                 <div className="mt-20 lg:mr-32">
-                    <p className="text-xl mb-3 font-bold tracking-widest">{t('LANGUAGES')}</p>
+                    <h2 className="text-xl mb-3 font-bold tracking-widest">{t('LANGUAGES')}</h2>
                     <List props={langs}/>
                 </div>
                 <div className="mt-20 lg:mr-32">
-                    <p className="text-xl mb-3 font-bold tracking-widest">{t('FRAMEWORKS')}</p>
+                    <h2 className="text-xl font-bold tracking-widest">{t('FRAMEWORKS')}</h2>
+                    <h2 className="text-xl mb-3 font-bold tracking-widest">{t('AND LIBRARIES')}</h2>
                     <List props={frameworks} />
                 </div>
                 <div className="mt-20 mb-10">
-                    <p className="text-xl mb-3 font-bold tracking-widest">{t('OTHERS')}</p>
+                    <h2 className="text-xl mb-3 font-bold tracking-widest">{t('OTHERS')}</h2>
                     <List props={others} />
                 </div>
             </section>
