3fae0ec0fb2085216d118fee7af06ea60d3c7ee9
Dmitrii Ivanushkin <dmitrii.unwritten183@passmail.net>
2023-02-13 19:16:10 +0100

    fix(About): styles

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { RxDividerVertical } from "react-icons/rx";
 import { langs, frameworks, others } from "../../data/skills";
 import List from "../reusable/List";
-import School from "../../img/school.png";
+import EduNode from "../reusable/EduNode";
 import Skive from "../../img/skive.png";
 import VUT from "../../img/vut.png";
 
@@ -12,7 +11,7 @@ function About() {
 
     return (
          <main className="mb-auto">
-            <h1 className="lg:text-6xl text-4xl text-center my-16">{t("Skills")}</h1>
+            <h1 className="lg:text-8xl text-6xl text-center my-20">{t("Skills")}</h1>
             <section className="lg:flex justify-center border-2 bg-white bg-opacity-5 border-white lg:mx-20 mx-5">
                 <div className="lg:w-1/3 lg:border-r-2 border-white m-16 lg:mx-0 lg:px-16 px-0">
                     <h2 className="text-2xl text-white mb-5 tracking-widest">{t("LANGUAGES")}</h2>
@@ -29,28 +28,13 @@ function About() {
                     <List props={others} />
                 </div>
             </section>
-            <h1 className="lg:text-6xl text-4xl text-center my-16">{t("Education")}</h1>
-            <section className="lg:flex mx-12">
-                <div className="lg:w-1/3 text-center">
-                    <h2 className="text-2xl text-white text-center tracking-widest">Business High School</h2>
-                    <p className="text-gray text-center">Russia</p>
-                    <p className="text-gray text-center">2018-2020</p>
-                    <img src={School} alt="School.png" loading="lazy" className="h-20 w-auto mx-auto my-10" />
-                </div>
-                <RxDividerVertical className="lg:hidden mx-auto" size={90} />
-                <div className="lg:w-1/3 text-center">
-                    <h2 className="text-2xl text-white text-center tracking-widest lg:mt-0 mt-10">Skive College</h2>
-                    <p className="text-gray text-center">Denmark</p>
-                    <p className="text-gray text-center">2019</p>
-                    <img src={Skive} alt="Skive-College.png" loading="lazy" className="h-20 w-auto mx-auto my-10" />
-                </div>
-                <RxDividerVertical className="lg:hidden mx-auto" size={90} />
-                <div className="lg:w-1/3 text-center">
-                    <h2 className="text-2xl text-white text-center tracking-widest lg:mt-0 mt-10">Brno University of Technology</h2>
-                    <p className="text-gray text-center">Czech Republic</p>
-                    <p className="text-gray text-center">2021-*</p>
-                    <img src={VUT} alt="VUT-FIT.png" loading="lazy" className="h-20 w-auto mx-auto my-10" />
-                </div>
+            <h1 className="lg:text-8xl text-6xl text-center my-20">{t("Education")}</h1>
+            <section className="lg:flex mb-20">
+                <EduNode school="Business High School" place="Russia" yrs="2018-2020" />
+                <div id="divider-hor1" className="lg:hidden mx-auto w-0.5 h-32 bg-white my-10"></div>
+                <EduNode school="Skive College" place="Denmark" yrs="2019" src={Skive} />
+                <div id="divider-hor2" className="lg:hidden mx-auto w-0.5 h-32 bg-white my-10"></div>
+                <EduNode school="Brno University of Technology" place="Czech Republic" yrs="2021-..." src={VUT} />
             </section>
         </main>
     );
---
+++ src/components/reusable/EduNode.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+
+function EduNode(props) {
+	const [image, setImage] = useState(false);
+
+	useEffect(() => {
+		if (props.src !== undefined){
+			setImage(true);
+		}
+	}, [props.src]);
+
+    const { t } = useTranslation();
+
+	return (
+		<div className="lg:w-1/3 lg:flex justify-center">
+            <div>
+                <h2 className="text-2xl text-white text-center tracking-widest">{t(props.school)}</h2>
+                <p className="text-gray text-center">{props.place}</p>
+                <p className="text-gray text-center">{props.yrs}</p>
+            </div>
+            { image && <img src={props.src} alt="logo" loading="lazy" className="h-20 w-auto lg:mx-10 mx-auto lg:mt-0 mt-5" />}
+        </div>
+    );
+}
+
+export default EduNode;
