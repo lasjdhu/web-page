20b53b49ddc745ac705ca873f6dbb1a36194a41b
Dmitrii Ivanushkin <dmitrii.ivanushkin1@gmail.com>
2023-03-08 06:46:14 +0100

    feat(Contact): Social Icons

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,42 +1,68 @@
-import React from "react";
-// import { SocialIcon } from 'react-social-icons';
+import React, { useState } from "react";
+import { SocialIcon } from "react-social-icons";
 import Badge from "../reusable/Badge";
 
 function Contact() {
-    return (
-    	<main className="m-auto">
-    		<Badge />
-	    	{/*<ul className="block pt-20">
-	            <li className="inline-block px-5">
-    				<p className="text-3xl">Social:</p>
-	            </li>
-	            <li className="inline-block px-5">
-	                <SocialIcon 
-		                url="https://linkedin.com/in/dmitrii-ivanushkin" 
-		                className="hover:scale-125 duration-300" 
-		                fgColor="white" 
-		                style={{ height: 40, width: 40 }} 
-	                />
-	            </li>
-	            <li className="inline-block px-5">
-	                <SocialIcon 
-	                	url="https://github.com/re-roll" 
-	                	className="hover:scale-125 duration-300" 
-	                	bgColor="white" 
-	                	style={{ height: 40, width: 40 }} 
-	                />
-	            </li>
-	            <li className="inline-block px-5">
-	                <SocialIcon 
-	                	url="https://twitter.com/rerollorer" 
-	                	className="hover:scale-125 duration-300" 
-	                	fgColor="white" 
-	                	style={{ height: 40, width: 40 }} 
-	                />
-	            </li>
-	        </ul>*/}
-        </main>
-    );
+ 	// const [name, setName] = useState("");
+	// const [email, setEmail] = useState("");
+  	// const [message, setMessage] = useState("");
+
+  	// const handleSubmit = (event) => {
+    // 	event.preventDefault();
+    // 	// Handle form submission
+  	// };
+
+  	return (
+  		<main className="m-auto">
+  			<Badge />
+			{/*<form className="flex flex-col" onSubmit={handleSubmit}>
+		  		<label>
+		    	Name:
+		        	<input
+		          		type="text"
+		          		value={name}
+		          		onChange={(event) => setName(event.target.value)}
+		        	/>
+		  		</label>
+			    <label>
+			    Email:
+			        <input
+			        	type="email"
+			        	value={email}
+			        	onChange={(event) => setEmail(event.target.value)}
+			        />
+			    </label>
+		  		<label>
+		    	Message:
+			        <textarea
+			        	value={message}
+			        	onChange={(event) => setMessage(event.target.value)}
+			        />
+		  		</label>
+		  	<button type="submit">Submit</button>
+			</form>*/}
+			<ul className="flex flex-row justify-center pt-10">
+ 	            <li className="">
+ 	                <SocialIcon
+ 		                url="https://linkedin.com/in/dmitrii-ivanushkin"
+ 		                className="hover:scale-125 duration-300"
+ 		                fgColor="white"
+ 		                bgColor="black"
+ 		                style={{ height: 40, width: 40 }}
+ 	                />
+ 	            </li>
+ 	            <li className="px-10">
+ 	                <SocialIcon
+ 	                	url="https://github.com/re-roll"
+ 	                	className="hover:scale-125 duration-300" 
+ 	                	fgColor="white"
+ 	                	bgColor="black"
+ 	                	style={{ height: 40, width: 40 }} 
+ 	                />
+ 	            </li>
+ 	        </ul>
+	 	</main>
+  	);
 }
 
 export default Contact;
