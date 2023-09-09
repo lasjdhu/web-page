22a336c626c07d1f9997b02a13ebfc3170eba33d
Dmitrii Ivanushkin <dmitrii.unwritten183@passmail.net>
2023-09-09 13:36:03 +0200

    ci: fix prettier

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -7,9 +7,22 @@ function Box(props) {
 			<h2 className="text-xl pt-5 mt-2">{props.title}</h2>
 			<p className="text-left text-light m-5">{props.desc}</p>
 			<div
-				className={`inline-block mt-2 mx-5 px-3 py-2 lg:mt-0 ${props.isEnabled ? 'bg-light text-midnight cursor-pointer transition ease-in-out hover:-translate-y-1 duration-300' : 'bg-transparent border border-dashed cursor-not-allowed'}`}
+				className={`inline-block mt-2 mx-5 px-3 py-2 lg:mt-0 ${
+					props.isEnabled
+						? 'bg-light text-midnight cursor-pointer transition ease-in-out hover:-translate-y-1 duration-300'
+						: 'bg-transparent border border-dashed cursor-not-allowed'
+				}`}
 			>
-				<NavLink to={props.to} className={`${props.isEnabled ? 'cursor-pointer': 'cursor-not-allowed'}`}>RUN</NavLink>
+				<NavLink
+					to={props.to}
+					className={`${
+						props.isEnabled
+							? 'cursor-pointer'
+							: 'cursor-not-allowed'
+					}`}
+				>
+					RUN
+				</NavLink>
 			</div>
 			<a
 				href={props.src}
