dba6a3948427f8cc2fd90c963d32b71ba64e4c2a
Dmitrii Ivanushkin <dmitrii.unwritten183@passmail.net>
2023-09-09 21:56:12 +0200

    build: migrate to vite

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,7 @@ import './i18n/config';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
--- src/pages/projects/GameOfLife.jsx
+++ src/pages/projects/GameOfLife.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function GameOfLife() {
 	React.useEffect(() => {
-		window.location.href = '/projects/game-of-life';
+		window.location.href = '/projects/game-of-life/index.html';
 	}, []);
 
 	return null;
