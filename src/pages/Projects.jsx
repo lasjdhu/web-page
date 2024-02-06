d9ea3f07b717f7f058dfefe0f8967b933f9e60f5
Dmitrii Ivanushkin <dmitrii.ivanushkin1@gmail.com>
2024-02-06 02:05:56 +0100

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,13 +2,12 @@ import { useTranslation } from "react-i18next";
 
 import Box from "../components/Box";
 import gol from "../assets/gol.png";
-import gnc from "../assets/gnc.png";
 
 export default function Projects() {
   const { t } = useTranslation();
 
   return (
-    <main className="m-auto lg:mt-16 lg:mb-20 mt-12 mb-12 flex flex-col">
+        <main className="m-auto flex justify-center align-center items-center">
       <Box
         title={t("gol")}
         img={gol}
