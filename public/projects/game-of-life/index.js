0639a7c886f1c1668d0d3f188183bfa9c084f818
Dmitrii Ivanushkin <dmitrii.unwritten183@passmail.net>
2024-04-14 04:15:40 +0200

    build: update game-of-life

--- public/projects/game-of-life/index.js
+++ public/projects/game-of-life/index.js
@@ -1,53 +1,89 @@
 const canvas = document.querySelector(".gameCanvas");
 const ctx = canvas.getContext("2d");
 
-const cellSize = 10;
+const cellSize = 15;
 const rows = ctx.canvas.width / cellSize;
 const cols = ctx.canvas.height / cellSize;
 
 const game = new Game(rows, cols);
 
 window.onload = () => {
-  function drawGrid() {
+  const drawGrid = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < game.rows; i++) {
-      for (let j = 0; j < game.cols; j++) {
-        const cell = game.getCell(i, j);
-        if (cell.isAlive()) {
-          ctx.fillStyle = "#fff";
+    game.grid.forEach((row, i) => {
+      row.forEach((cell, j) => {
+        if (cell.alive) {
+          ctx.fillStyle = "rgba(255, 255, 255, 1)";
+          ctx.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
+        } else {
+          ctx.fillStyle = "rgba(255, 255, 255, 0)";
           ctx.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
         }
-      }
-    }
-  }
+      });
+    });
+  };
 
   let running = false;
+  let drawing = false;
 
-  const random = document.querySelector(".randomizeButton");
-  random.addEventListener("click", () => {
-    game.randomize();
-  });
+  const handleMouseDown = (event) => {
+    drawing = true;
+    handleMouseMove(event);
+  };
 
-  document.querySelector(".clearButton").addEventListener("click", () => {
-    game.clear();
-  });
+  const handleMouseUp = () => {
+    drawing = false;
+  };
 
-  document.querySelector(".stopButton").addEventListener("click", () => {
+  const handleMouseMove = (event) => {
+    if (!drawing) return;
+    const rect = canvas.getBoundingClientRect();
+    const x = Math.floor((event.clientX - rect.left) / cellSize);
+    const y = Math.floor((event.clientY - rect.top) / cellSize);
+    game.grid[x][y].alive = true;
+  };
+
+  const handleRandomize = () => {
     running = false;
-  });
+    game.randomize();
+  };
 
-  document.querySelector(".startButton").addEventListener("click", () => {
+  const handleClear = () => {
+    running = false;
+    game.clear();
+  };
+
+  const handleStop = () => {
+    running = false;
+  };
+
+  const handleStart = () => {
     running = true;
-  });
+  };
 
-  function gameLoop() {
-    if (running) game.nextGeneration();
-  }
+  const gameLoop = () => {
+    if (running) {
+      game.nextGeneration();
+      if (game.allCellsDead()) {
+        running = false;
+      }
+    }
+  };
 
-  function drawLoop() {
+  const drawLoop = () => {
     drawGrid();
     requestAnimationFrame(drawLoop);
-  }
+  };
+
+  document
+    .querySelector(".randomizeButton")
+    .addEventListener("click", handleRandomize);
+  document.querySelector(".clearButton").addEventListener("click", handleClear);
+  document.querySelector(".stopButton").addEventListener("click", handleStop);
+  document.querySelector(".startButton").addEventListener("click", handleStart);
+  canvas.addEventListener("mousedown", handleMouseDown);
+  canvas.addEventListener("mouseup", handleMouseUp);
+  canvas.addEventListener("mousemove", handleMouseMove);
 
   setInterval(gameLoop, 500);
   window.requestAnimationFrame(drawLoop);
