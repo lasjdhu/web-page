66f6e2c32c718a3c16caef8ab4fe07960698b5fa
Dmitrii Ivanushkin <dmitrii.unwritten183@passmail.net>
2025-05-02 01:45:17 +0200

    fix: use fallback if webgl is disabled (#41)
    * fix: use fallback if webgl is disabled
    
    * fix: use new responsive images enum

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -2,9 +2,20 @@ import "../styles/global.css";
 import { useThreeScene } from "../utils/useThreeScene";
 
 export default function Model() {
-  const { loading } = useThreeScene({
+  const { loading, error, webGLSupported } = useThreeScene({
     modelPath: "/duck.glb",
   });
+
+  if (!webGLSupported || error) {
+    return (
+      <div
+        id="model-container"
+        className="w-16 h-16 md:w-24 md:h-24 flex items-center justify-center transition-opacity duration-700"
+      >
+        <img src="/favicon.ico" />
+      </div>
+    );
+  }
 
   return (
     <div
